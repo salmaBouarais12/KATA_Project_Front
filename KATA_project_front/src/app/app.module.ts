b6126a086c22809f8cdaf5d6bf251ad93ede2eb9
SalmaBouarais <salmabouarais@fuwear-technology.com>
2023-09-13 16:12:52 +0200

    add component for users and header

--- KATA_project_front/src/app/app.module.ts
+++ KATA_project_front/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
